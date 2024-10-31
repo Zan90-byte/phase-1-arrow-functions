0bc3a857fdba5b4030da254b71fb4c338a0825a3
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2024-10-31 14:50:08 -0700

    lab done

---
+++ index.js
@@ -0,0 +1,11 @@
+const value1 = 2000;
+const value2 = 100;
+const divide = (value1, value2) => value1 / value2;
+
+//****************************************** */
+
+const square = value => value * value;
+
+//**************************************** */
+
+const add = (value1, value2)  => value1 + value2;
